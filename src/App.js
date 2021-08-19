63f22e67fd4894cf6b4b81e3e3a55fd76a376106
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-19 20:17:29 +0530

    Created MainPageLayout and Title files and made some changes in esLint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Error: 404 Page not found</div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Error: 404 Page not found</div>
+      </Route>
+    </Switch>
   );
 }
 
