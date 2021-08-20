1100a53f784cfb82e25a31a716b5b0f66447e9dc
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-20 16:59:56 +0530

    Added the style-components to our project

--- src/Components/show/ShowGrid.js
+++ src/Components/show/ShowGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ShowCard from './ShowCard';
 import IMAGE_NOT_FOUND from '../../Images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const ShowGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({ show }) => (
         <ShowCard
           key={show.id}
@@ -13,7 +14,7 @@ const ShowGrid = ({ data }) => {
           image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
