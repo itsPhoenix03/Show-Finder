c67f1aa84855215420617ec6db4d0bd1f111f05e
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-22 14:52:22 +0530

    Added and removed show id to local storage and styled the star using dynamic styling

--- src/Components/show/ShowGrid.js
+++ src/Components/show/ShowGrid.js
@@ -2,18 +2,32 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import IMAGE_NOT_FOUND from '../../Images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShow } from '../../misc/custom-hook';
 
 const ShowGrid = ({ data }) => {
+  const [starred, dispatchStarred] = useShow();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starred.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) dispatchStarred({ type: 'REMOVE', showId: show.id });
+          else dispatchStarred({ type: 'ADD', showId: show.id });
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
