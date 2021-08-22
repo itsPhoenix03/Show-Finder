d6c719848d043c4f7787c02a1f5f5707cf7a342c
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-22 17:34:16 +0530

    Applied styles to the pages: Title, Navbar, Theme prop for using and using useLocation hook

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { LinkStyled, NavList } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
@@ -7,15 +8,23 @@ const LINKS = [
 ];
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div>
-      <ul>
+      <NavList>
         {LINKS.map(items => (
           <li key={items.to}>
-            <Link to={items.to}> {items.text} </Link>
+            <LinkStyled
+              to={items.to}
+              className={items.to === location.pathname ? 'active' : ''}
+            >
+              {' '}
+              {items.text}{' '}
+            </LinkStyled>
           </li>
         ))}
-      </ul>
+      </NavList>
     </div>
   );
 };
