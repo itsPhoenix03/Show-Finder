ae5b30cec49e503cdc4b437ada61b9b648ede088
Shreyas-Misra <shreyas.misra03@gmail.com>
2021-08-22 16:00:59 +0530

    Rendered results on the starred page

--- src/Pages/Starred.js
+++ src/Pages/Starred.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
+import { useShow } from '../misc/custom-hook';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../Components/show/ShowGrid';
+
+function reducer(prevState, action) {
+  switch (action.type) {
+    case 'RENDER': {
+      return { ...prevState, shows: action.show, isLoading: false };
+    }
+    case 'ERROR': {
+      return { ...prevState, error: action.error, isLoading: false };
+    }
+    default:
+      return { ...prevState, isLoading: false };
+  }
+}
+
+const intialState = {
+  shows: null,
+  isLoading: true,
+  error: null,
+};
 
 const Starred = () => {
-  return <MainPageLayout>This is Starred</MainPageLayout>;
+  const [starred] = useShow();
+  const [{ shows, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(result => dispatch({ type: 'RENDER', show: result }))
+        .catch(err => dispatch({ type: 'ERROR', error: err.massage }));
+    } else dispatch({ type: '' });
+  }, [starred]);
+  return (
+    <MainPageLayout>
+      {isLoading && (
+        <div>
+          Favourite Shows are being Loading.... <br />
+          Plaese wait for a while!!
+        </div>
+      )}
+      {error && <div>Error Occurred: {error}</div>}
+      {!isLoading && !shows && (
+        <div>No Show has been added to favourites!!!</div>
+      )}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
